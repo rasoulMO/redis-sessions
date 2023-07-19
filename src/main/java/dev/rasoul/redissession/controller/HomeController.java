0df812d14bd8d436728fedea7aadaee72f365668
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2023-07-19 15:15:13 +0200

    added docker compone & redis

--- src/main/java/dev/rasoul/redissession/controller/HomeController.java
+++ src/main/java/dev/rasoul/redissession/controller/HomeController.java
@@ -1,5 +1,6 @@
 package dev.rasoul.redissession.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,22 @@ import java.security.Principal;
 @RestController
 public class HomeController {
 
+	private final String HOME_VIEW_COUNT = "homeViewCount";
+
 	@GetMapping("/")
-	public String home(Principal principal) {
+	public String home(Principal principal, HttpSession session) {
+		incrementViewCount(session, HOME_VIEW_COUNT);
 		return "Hello World! " + principal.getName();
+	}
+
+	@GetMapping("/home")
+	public String home2(HttpSession session) {
+		incrementViewCount(session, HOME_VIEW_COUNT);
+		return "homeViewCount " + session.getAttribute(HOME_VIEW_COUNT);
+	}
+
+	private void incrementViewCount(HttpSession session, String attr) {
+		var viewCount =  session.getAttribute(attr) == null ? 0 : (Integer)session.getAttribute(attr);
+		session.setAttribute(attr, ++viewCount);
 	}
 }
